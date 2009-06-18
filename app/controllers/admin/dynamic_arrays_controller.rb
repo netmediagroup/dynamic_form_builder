76fdd77df41098e6b04421efdb2409aa6d6594c1
Jared Howard <jhoward@netmediagroup.com>
2009-06-18 17:46:19 -0600

    added admin controller dynamic_arrays

---
+++ app/controllers/admin/dynamic_arrays_controller.rb
@@ -0,0 +1,57 @@
+class Admin::DynamicArraysController < Admin::AdminController
+  before_filter :load_arrays, :only => [:index]
+  before_filter :load_array, :only => [:show, :edit, :update, :destroy]
+  before_filter :load_new_array, :only => [:new, :create]
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    if @array.save
+      flash[:notice] = 'Array was created.'
+      redirect_to [:admin, @array]
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @array.update_attributes(params[:dynamic_array])
+      flash[:notice] = 'Array was updated.'
+      redirect_to [:admin, @array]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @array.destroy
+    flash[:notice] = 'Array was deleted.'
+    redirect_to admin_arrays_path
+  end
+
+
+protected
+
+  def load_arrays
+    @arrays = DynamicArray.all(:order => 'name ASC')
+  end
+
+  def load_array
+    @array = DynamicArray.find(params[:id])
+  end
+
+  def load_new_array
+    @array = params[:dynamic_array].nil? ? DynamicArray.new : DynamicArray.new(params[:dynamic_array])
+  end
+
+end
