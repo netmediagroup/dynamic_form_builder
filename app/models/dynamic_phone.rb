8badc6e2230fc2df0f2ba6c56ad9328be7c5e341
Jared Howard <jhoward@netmediagroup.com>
2009-08-18 12:52:23 -0600

    update to DynamicPhone of separate inputs to accept param field for a single input

--- app/models/dynamic_phone.rb
+++ app/models/dynamic_phone.rb
@@ -7,7 +7,14 @@ class DynamicPhone < ActiveRecord::Base
     column_name = self.dynamic_field.column_name
     if self.separate_inputs
       ['area','prefix','suffix'].each do |phone_type|
-        attrs[phone_type.to_sym] = params["#{column_name}_#{phone_type}"]
+        value = params["#{column_name}_#{phone_type}"]
+        value = case phone_type
+          when 'area' then params[column_name][0..2]
+          when 'prefix' then params[column_name][3..5]
+          when 'suffix' then params[column_name][6..9]
+        end if value.nil? && !params[column_name].blank?
+
+        attrs[phone_type.to_sym] = value
         attrs["#{phone_type}_prompt".to_sym] = ''
       end
     end
@@ -17,12 +24,13 @@ class DynamicPhone < ActiveRecord::Base
 
   def field_value(params={})
     column_name = self.dynamic_field.column_name
+
+    value = params[column_name.to_s]
     if self.separate_inputs
       merged = (params["#{column_name}_area"].to_s + params["#{column_name}_prefix"].to_s + params["#{column_name}_suffix"].to_s)
-      return merged.blank? ? nil : merged
-    else
-      params[column_name.to_s]
+      value = merged unless merged.blank?
     end
+    return value
   end
 
 end
