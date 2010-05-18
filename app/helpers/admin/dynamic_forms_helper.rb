bdcaadc96551e4ba375c5ca5535bd5390411c4dc
Jared Howard <jhoward@netmediagroup.com>
2010-05-18 15:31:36 -0600

    needs parentheses for future versions

--- app/helpers/admin/dynamic_forms_helper.rb
+++ app/helpers/admin/dynamic_forms_helper.rb
@@ -3,7 +3,7 @@ module Admin::DynamicFormsHelper
   # I know this is an ugly way to code this, but right now it doesn't matter.
   def render_partial_if_exists(partial)
     view_paths.each do |path|
-      return render partial if File.exists?(File.join(path, params[:controller], "_#{partial}.html.erb"))
+      return render(partial) if File.exists?(File.join(path, params[:controller], "_#{partial}.html.erb"))
     end
     return nil
   end
